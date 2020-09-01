b0058216f9c81d89becf0c8ab292546b4791c3ef
gdickey273 <gdickey273@gmail.com>
2020-09-01 10:28:03 -0400

    Forgot to commit. Coded promptName() promptEmail() and promptRole() functions in app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
@@ -13,6 +15,146 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+console.log("Welcome to the app!")
+const promptEmployeeInfo = async () => {
+
+  let name;
+  let email;
+  let role;
+  
+  const promptName = async () => {
+
+    let isValidName;
+
+    await inquirer.prompt([
+      {
+        name: "name",
+        type: "input",
+        message: "Please enter employee name."
+      },
+    ]).then((answers) => {
+      if (!/[^abcdefghijklmnopqrstuvwxyz-\s]/.test(answers.name.toLowerCase())) {
+        isValidName = true;
+        name = answers.name;
+      } else isValidName = false;
+    });
+
+
+    while (!isValidName) {
+      await inquirer.prompt([{
+        message: "Invalid name. Please use only letters A-Z and '-' characters.",
+        type: "input",
+        name: "name"
+      }]).then(answers => {
+        if (!/[^abcdefghijklmnopqrstuvwxyz-\s]/.test(answers.name.toLowerCase())) {
+          isValidName = true;
+          name = answers.name;
+        }
+      });
+    }
+
+  }
+
+  const promptEmail = async () => {
+
+    let isValidEmail;
+
+    await inquirer.prompt([
+      {
+        name: "email",
+        type: "input",
+        message: "Please enter employee email."
+      },
+    ]).then((answers) => {
+      if(answers.email.split("@").length !== 2){
+        isValidEmail = false;
+      } else if(answers.email.trim().split(" ").length > 1){
+        isValidEmail = false;
+      } else if(answers.email.split("@")[0].split(".").length > 0 || answers.email.split("@")[1].split(".").length < 1){
+        isValidEmail = false;
+      } else{
+        isValidEmail = true;
+        email = answers.email;
+      }
+    });
+
+
+    while (!isValidEmail) {
+      await inquirer.prompt([{
+        message: "Please enter a valid email address.",
+        type: "input",
+        name: "email"
+      }]).then((answers) => {
+        if(answers.email.split("@").length !== 2){
+          isValidEmail = false;
+        } else if(answers.email.trim().split(" ").length > 1){
+          isValidEmail = false;
+        } else if(answers.email.split("@")[0].split(".").length > 1 || answers.email.split("@")[1].split(".").length < 2){
+          isValidEmail = false;
+        } else{
+          isValidEmail = true;
+          email = answers.email;
+        }
+      });
+  
+    }
+
+  }
+
+
+  const promptRole = async () => {
+
+    await inquirer.prompt([
+      {
+        name: "role",
+        type: "list",
+        message: "Please select employee role.",
+        choices: [{ name: "Manager", value: "manager" }, { name: "Engineer", value: "engineer" }, { name: "Intern", value: "intern" }]
+      },
+    ]).then((answers) => {
+      role = answers.role;
+    });
+
+  }
+
+
+  await promptName();
+  console.log(name);
+
+  await promptEmail();
+  console.log(email);
+
+
+  const promptManagerInfo = () => {
+    let name;
+    let email;
+    let officeNumber;
+
+
+  }
+
+
+  let areMoreEmployees = true;
+  while (areMoreEmployees) {
+    await promptRole();
+    console.log(role);
+    switch (role) {
+      case "manager":
+        promptManagerInfo();
+        break;
+      case "intern":
+        promptInterInfo();
+        break;
+      case "engineer":
+        promptEngineerInfo();
+        break;
+    };
+    console.log("After switch!");
+    areMoreEmployees = false;
+    //areMoreEmployees = promptMoreEmployees();
+  }
+}
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -33,3 +175,5 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+promptEmployeeInfo();
